8281bf2928950b6a86b7e5d8461d1dcbbcab39a2
Devon Domercq <djdomercq@gmail.com>
2017-04-19 05:13:24 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Shinobu Shabu - Practice restaurant website
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def home
+  end
+  
+  def about
+  end
+  
+  def menu
+  end
+  
+  def reservations
+  end
+  
+  def location
+  end
+  
+  def contact
+  end
+end
